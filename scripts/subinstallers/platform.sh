baa8f499a12d8deccb1b91bf462c9074aa042849
Steven Foerster <sfoerster@gmail.com>
2020-04-22 19:38:44 +0000

    Resolve "Support Ubuntu 20.04 LTS"

--- scripts/subinstallers/platform.sh
+++ scripts/subinstallers/platform.sh
@@ -4,11 +4,14 @@
 # Determine OS platform
 UNAME=$(uname | tr "[:upper:]" "[:lower:]")
 DISTRO=""
+VERSION_ID=""
 # If Linux, try to determine specific distribution
 if [ "$UNAME" == "linux" ]; then
     # use /etc/os-release to get distro 
     DISTRO=$(cat /etc/os-release | awk -F= '/^ID=/{print $2}')
+    VERSION_ID=$(cat /etc/os-release | awk -F= '/^VERSION_ID=/{print $2}' | tr -d '"')
 fi
 
 figlet "UNAME: $UNAME"
 figlet "DISTRO: $DISTRO"
+figlet "VERSION: $VERSION_ID"
