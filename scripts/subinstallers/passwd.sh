a57c529c7327f6445568fb0ba7a4a35e13c9e1f0
Steven Foerster <steven@cyber5k.com>
2021-03-16 16:52:46 -0400

    refactor

--- scripts/subinstallers/passwd.sh
+++ scripts/subinstallers/passwd.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
 # INPUT default admin password
-while [ -z "${MISTBORN_DEFAULT_PASSWORD}" ]; do
+while [ -z "${MISTBORN_DEFAULT_PASSWORD}" ] || [ ${MISTBORN_DEFAULT_PASSWORD} =~ ^[A-Za-z0-9_]+$ ]; do
     echo
     echo "(Mistborn) The default admin password may only container alphanumeric characters and _"
     read -p "(Mistborn) Set default admin password: " -s MISTBORN_DEFAULT_PASSWORD
     echo
-
-    if [[ ${MISTBORN_DEFAULT_PASSWORD} =~ ^[A-Za-z0-9_]+$ ]]; then
-    # it matches
-        echo "(Mistborn) Password is accepted"
-    else
-        unset MISTBORN_DEFAULT_PASSWORD
-        echo "(Mistborn) Try again"
-    fi
 
 done
 
