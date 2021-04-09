45ee4c7150b643a634361ac1c4078616e86ee0b8
Steven Foerster <steven@cyber5k.com>
2021-04-09 09:40:39 -0400

    scirius

--- scripts/subinstallers/extra/scirius.sh
+++ scripts/subinstallers/extra/scirius.sh
@@ -4,6 +4,6 @@
 SCIRIUS_PROD_FILE="$1"
 SCIRIUS_SECRET_KEY=$(python3 -c "import secrets; import string; print(f''.join([secrets.choice(string.ascii_letters+string.digits) for x in range(50)]))")
 echo "SECRET_KEY=$SCIRIUS_SECRET_KEY" > $SCIRIUS_PROD_FILE
-echo "ALLOWED_HOSTS=suricata.mistborn" >> $SCIRIUS_PROD_FILE
+echo "ALLOWED_HOSTS=scirius.mistborn" >> $SCIRIUS_PROD_FILE
 echo "DJANGO_SUPERUSER_USERNAME=mistborn" >> $SCIRIUS_PROD_FILE
 echo "DJANGO_SUPERUSER_EMAIL=mistborn@email.mistborn" >> $SCIRIUS_PROD_FILE
