fab092ad6c0394c0de1fdacdba54cb20f8f91029
Steven Foerster <steven@cyber5k.com>
2021-04-14 23:19:10 -0400

    tweaks

--- scripts/services/wazuh/init.sh
+++ scripts/services/wazuh/init.sh
@@ -9,6 +9,6 @@ mkdir -p /opt/mistborn_volumes/extra/wazuh/init/ >/dev/null 2>&1
 chmod -R +x /opt/mistborn_volumes/extra/wazuh/init/
 cp /opt/mistborn/scripts/services/wazuh/files/internal_users.yml /opt/mistborn_volumes/extra/wazuh/init/
 
-WAZUH_MISTBORN_HASHED=$(docker run --rm -ti amazon/opendistro-for-elasticsearch:1.12.0 bash /usr/share/elasticsearch/plugins/opendistro_security/tools/hash.sh -p "${MISTBORN_DEFAULT_PASSWORD}")
+WAZUH_MISTBORN_HASHED=$(docker run --rm -ti amazon/opendistro-for-elasticsearch:1.12.0 bash /usr/share/elasticsearch/plugins/opendistro_security/tools/hash.sh -p "${MISTBORN_DEFAULT_PASSWORD}" | tr -d '\n')
 
-sed -i "s/__MISTBORN_HASH__/${WAZUH_MISTBORN_HASHED}/" /opt/mistborn_volumes/extra/wazuh/init/internal_users.yml
+sed -i "s|__MISTBORN_HASH__|${WAZUH_MISTBORN_HASHED}|" /opt/mistborn_volumes/extra/wazuh/init/internal_users.yml
