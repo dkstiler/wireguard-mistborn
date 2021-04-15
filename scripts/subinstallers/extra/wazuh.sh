21c1388c1d884dc9317c6a8b0fee1609c282c328
Steven Foerster <steven@cyber5k.com>
2021-04-15 08:52:19 -0400

    ip

--- scripts/subinstallers/extra/wazuh.sh
+++ scripts/subinstallers/extra/wazuh.sh
@@ -11,12 +11,12 @@ echo "ELASTICSEARCH_PASSWORD=$MISTBORN_DEFAULT_PASSWORD" >> $WAZUH_PROD_FILE
 # kibana odfe
 # kibana-odfe/config/wazuh_app_config.sh
 # https://wazuh
-echo "WAZUH_API_URL=https://home.mistborn" >> $WAZUH_PROD_FILE
+echo "WAZUH_API_URL=https://10.2.3.1" >> $WAZUH_PROD_FILE
 echo "API_PORT=55000" >> $WAZUH_PROD_FILE
 
 # kibana-odfe/config/entrypoint.sh:
 # https://elasticsearch:9200
-echo "ELASTICSEARCH_URL=https://home.mistborn:9200" >> $WAZUH_PROD_FILE
+echo "ELASTICSEARCH_URL=https://10.2.3.1:9200" >> $WAZUH_PROD_FILE
 
 echo "MISTBORN_DEFAULT_PASSWORD=$MISTBORN_DEFAULT_PASSWORD" >> $WAZUH_PROD_FILE
 
