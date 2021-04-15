d6641f295bf1cf39601af2319e59c0a7b78e133c
Steven Foerster <steven@cyber5k.com>
2021-04-15 09:18:37 -0400

    wazuh agent

---
+++ scripts/services/wazuh/agent.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# detect if already installed
+if [ $(dpkg -s wazuh-agent &> /dev/null) -eq 0 ]; then
+    echo "Wazuh agent already installed"
+    exit 0
+fi
+
+# prepare repo
+echo "Adding Wazuh Repository"
+curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
+echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
+
+apt-get update
+
+# wait for service to be listening
+while ! nc -z 10.2.3.1 55000; do
+    WAIT_TIME=10
+    echo "Waiting ${WAIT_TIME} seconds for Wazuh API..."
+    sleep ${WAIT_TIME}
+done
+
+# install
+echo "Installing Wazuh agent"
+WAZUH_MANAGER="10.2.3.1" apt-get install wazuh-agent
